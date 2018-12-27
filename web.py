22dc0a7771c8da9d2d4a75e1e230ba722868af20
gymaisyl <42645486+gymaisyl@users.noreply.github.com>
2018-12-27 19:38:47 +0800

    18-数据库操作，一对多关系设置

--- web.py
+++ web.py
@@ -1,38 +1,42 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
+"""
+安装如下：
+flask-sqlalchemy
+flask-mysqldb
+
+"""
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1:3306/test'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
     return 'hello world'
 
-@app.route("/demo1")
-def demo1():
-    my_list = [
-        {
-            "id": 1,
-            "value": "我爱工作"
-        },
-        {
-            "id": 2,
-            "value": "工作使人快乐"
-        },
-        {
-            "id": 3,
-            "value": "沉迷于工作无法自拔"
-        },
-        {
-            "id": 4,
-            "value": "日渐消瘦"
-        },
-        {
-            "id": 5,
-            "value": "以梦为马，越骑越傻"
-        }
-    ]
 
-    return render_template("templates.html", date=my_list)
+class Role(db.Model):
+    """模型类创建"""
+    __tablename__ = 'roles'  # 定义数据库表名
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    # 这行代码　实现 user.role 和role.users 关联之间的相互数据取出
+    users = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+    """模型类创建"""
+    __tablename__ = 'users'  # 定义数据库表名
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    # 添加外键　
+    role_id = db.Column(db.Integer, db.ForeignKey(Role.id))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
